a6fa87ce89ccf2d1e4c74f4c98e707dbb00d7f3a
Dhegnaas <abdirahman460930@gmail.com>
2025-12-07 10:06:49 +0300

    second commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ImageBackground, ScrollView } from 'react-native'
+const logoImg = require ("../../assets/images/splash-icon.png")
 
 export default function Layout() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>soo dhawaaw muuse nin daalan aa tehee</Text>
-
-      <View 
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      />
-
-      <View 
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      />
-
-      <View 
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <ScrollView>
+      <Image source={logoImg} style={{ width:300, height:300 }} />
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Natus molestias quod sit expedita cupiditate quos eligendi 
+        neque possimus repellat inventore sed incidunt, et iusto 
+        tenetur hic consectetur dicta. Dolor, praesentium!
+      </Text>
+       <Image source={logoImg} style={{ width:300, height:300 }} />
+       </ScrollView>
     </View>
   );
 }
+ 
